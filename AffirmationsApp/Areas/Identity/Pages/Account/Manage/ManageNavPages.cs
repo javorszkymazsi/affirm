cc0d6ff0f61bdc26fc802e1a799cb7430b4e3238
Jávorszky Mazsi <javorszky.mariann@gmail.com>
2025-05-28 11:18:24 +0200

    add Manage/Layout scaffolded Identity pages

--- AffirmationsApp/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ AffirmationsApp/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -59,8 +59,7 @@ namespace  AffirmationsApp.Areas.Identity.Pages.Account.Manage
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        /// DISABLED. Left here for potential future use.
-        //public static string TwoFactorAuthentication => "TwoFactorAuthentication";
+        public static string TwoFactorAuthentication => "TwoFactorAuthentication";
 
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -108,8 +107,7 @@ namespace  AffirmationsApp.Areas.Identity.Pages.Account.Manage
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        /// DISABLED. Left here for potential future use.
-        //public static string TwoFactorAuthenticationNavClass(ViewContext viewContext) => PageNavClass(viewContext, TwoFactorAuthentication);
+        public static string TwoFactorAuthenticationNavClass(ViewContext viewContext) => PageNavClass(viewContext, TwoFactorAuthentication);
 
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
